b68528ca8696f67077c64415ed73a1331e147348
RamSuthar-Digia <ramsuthar@digia.tech>
2026-04-18 12:37:56 +0530

    Remove WebEngage integration  from iOS

--- iOS/Sources/DigiaCEP.swift
+++
@@ -1,5 +0,0 @@
-// All CEP types (InAppPayload, InAppPayloadContent, DigiaCEPPlugin, DigiaCEPDelegate,
-// DigiaExperienceEvent, DiagnosticReport, JSONValue) are provided by the DigiaEngage module.
-//
-// Import DigiaEngage in your source files:
-//   import DigiaEngage
